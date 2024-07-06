b5556c8001cb5b04b1fe1eb7ba4397b6f38e41c0
Pari Chauhan <123296817+Pari-Chauhan@users.noreply.github.com>
2024-07-07 01:43:57 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,6 @@
-// Immediately Invoked Function Expressions (IIFE)
-
-
+// Immediately Invoked Function Expressions (IIFE) - executes immediately
+//Global scope's pollution causes problem to prevent the problem of global scope variables we have used IIFE
+//() -> to wrap function, () to make execution call
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
