1976cad0ab43797a1d1861a65b007fc328a4a1ed
Pari Chauhan <123296817+Pari-Chauhan@users.noreply.github.com>
2024-07-07 01:36:52 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,7 +4,7 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(this);  //print the username object
     }
 
 }
@@ -13,23 +13,25 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this); 
+//In node environment we don't have any global objects hence it will print null
+//But if we write this in web browser then it will print the window object as for browser window is the global object
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); //this inside funtion has no reference
 // }
 
 // chai()
 
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); //this inside funtion has no reference
 // }
 
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this); //this inside funtion has no reference
 }
 
 
@@ -39,7 +41,7 @@ const chai =  () => {
 //     return num1 + num2
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2 //implicit return when we have only 1 statment inside function and that too of return value
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
